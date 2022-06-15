9632ddaed9aa71dbc6855c22e02be4a03898b72c
ethan-lee2 <106037045+ethan-lee2@users.noreply.github.com>
2022-06-15 15:31:49 -0400

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./styles.css";
+import ToggleDays from "./ToggleDays";
+
+export default function App() {
+  return (
+    <div className="App">
+      <p>Days of the week</p>
+      <ToggleDays />
+    </div>
+  );
+}
